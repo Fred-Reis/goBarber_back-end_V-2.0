f8653cd204fa110482135c2f4b89edb050adca66
Fred-Reis <fredgost@hotmail.com>
2020-04-19 02:24:43 -0300

    DB integration

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,6 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,16 +10,12 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const appointmentAlredyExist = this.appointmentsRepository.findByDate(
+    const appointmentAlredyExist = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -25,10 +23,12 @@ class CreateAppointmentService {
       throw Error('Appointment alredy exists!');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }

edc01122b256bb31840a56f39d07e95240b7c2dd
Fred-Reis <fredgost@hotmail.com>
2020-07-06 23:01:21 -0300

    feat: providers routes done

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('listProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'testador',
+      email: 'testador@teste.com',
+      password: '123456',
+    });
+
+    const user1 = await fakeUsersRepository.create({
+      name: 'testador1',
+      email: 'testador1@teste.com',
+      password: '123456',
+    });
+
+    const logged = await fakeUsersRepository.create({
+      name: 'testador2',
+      email: 'testador2@teste.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: logged.id,
+    });
+
+    expect(providers).toEqual([user, user1]);
+  });
+});
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
@@ -8,6 +9,7 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
